2ce80b952416d5b163f6a2cf364358f23e42b668
merrymeerkat <54228026+merrymeerkat@users.noreply.github.com>
2020-05-02 23:08:14 -0300

    Update 2_sum.ml

--- 2_sum.ml
+++ 2_sum.ml
@@ -1,3 +1,8 @@
+(*Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.*)
+
+
 let rec two_sum4 arr int =
   match arr with
   |h::t ->
