25cd9528fccf083448741f3841c09dfd54b9c138
merrymeerkat <54228026+merrymeerkat@users.noreply.github.com>
2020-05-07 03:50:26 +0200

    Update 2_sum.ml

--- 2_sum.ml
+++ 2_sum.ml
@@ -2,16 +2,24 @@
 
 You may assume that each input would have exactly one solution, and you may not use the same element twice.*)
 
+let rec drop_first l =
+  match l with
+    h::t-> t
+  |[]->[];;
 
-let rec two_sum4 arr int =
+let rec two_sixm arr int =
   match arr with
   |h::t ->
     (match t with
-     |t'::t''->
-      (match t'' with
-       |y::y' ->  if (h + t' == int) then Some(h, t')
-                  else two_sum4 (h::t'') int
-       |[] -> if (h + t' == int) then Some(h, t')
-              else (two_sum4 t int))
+    |t'::t''->
+      if (h + t' == int) then Some(h, t') else two_sixm (h::t'') int
     |[]-> None)
   |[]-> None;;
+
+let another_one arr int =
+  if (two_sixm arr int != None) then
+    two_sixm arr int
+ else two_sixm (drop_one arr) int;;
+
+
+  
