f0e1130e715c30268ba495c69082fc4a694c5686
merrymeerkat <54228026+merrymeerkat@users.noreply.github.com>
2020-05-13 03:52:00 +0200

    Update two_sum.ml

--- two_sum.ml
+++ two_sum.ml
@@ -4,6 +4,7 @@ You may assume that each input would have exactly one solution, and you may not 
 
 Level: Easy*)
 
+(* transforms int list into tuple list. The first element of each tuple is an index, the second is the original int. e.g. [3;4;5] becomes [(0,2);(1,4);(2,5)]*)
 let tuplefy l =
   let rec helper l counter =
     match l with
@@ -11,21 +12,24 @@ let tuplefy l =
   |[] -> []
   in helper l 0;;
 
-let rec helper_sum arr int =
+(*Checks whether the first int of a tuple list, when summed with any of the remaining ints in the tuple list, adds up to the target int. Returns Some(index first int, index second int) if they add up, None otherwise*)
+let rec helper_sum arr target_int =
   match arr with
   |(i,h)::t ->
     (match t with
     |(i',t')::t''->
-      if (h + t' == int) then Some(i, i') else helper_sum ((i,h)::t'') int
+      if (h + t' == target_int) then Some(i, i') else helper_sum ((i,h)::t'') target_int
     |[]-> None)
   |[]-> None;;
 
-let rec two_sum arr int =
-  if (helper_sum arr int != None) then
-    helper_sum arr int
-  else two_sum (drop_first arr) int;;
+(*runs helper_sum for each tuple in the tuple list. If the head tuple returns None, we drop the head tuple and execute helper_sum again, and so on. Function stops when Some(index, index) is returned, or when we reach an empty tuple list (meaning that none of the numbers add up to the target int, in which case we return None*)
+let rec two_sum arr target_int =
+  if (helper_sum arr tagert_int != None) then
+    helper_sum arr target_int
+  else two_sum (drop_first arr) target_int;;
 
-let two_sum arr int = another_one (tuplefy arr) int;;
+(*This ensures the user input (an int list) will be passed as a tuple list with indexes *)
+let two_sum arr target_int = two_sum (tuplefy arr) target_int;;
 
 two_sum [1;2;3] 5;;
 two_sum [2;7;11;15] 9;;
