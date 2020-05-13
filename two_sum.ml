aed50dd0d36ca285dfdf366415c21a1422798432
merrymeerkat <54228026+merrymeerkat@users.noreply.github.com>
2020-05-13 03:42:15 +0200

    Update two_sum.ml

--- two_sum.ml
+++ two_sum.ml
@@ -5,30 +5,31 @@ You may assume that each input would have exactly one solution, and you may not 
 Level: Easy*)
 
 let tuplefy l =
-let rec helper l counter =
-  match l with
-  h::t -> (counter, h) :: helper t
+  let rec helper l counter =
+    match l with
+  |h::t -> (counter, h) :: helper t (counter + 1)
   |[] -> []
   in helper l 0;;
 
-let rec drop_first l =
-  match l with
-    h::t-> t
-  |[]->[];;
-
-let rec two_sixm arr int =
+let rec helper_sum arr int =
   match arr with
-  |h::t ->
+  |(i,h)::t ->
     (match t with
-    |t'::t''->
-      if (h + t' == int) then Some(h, t') else two_sixm (h::t'') int
+    |(i',t')::t''->
+      if (h + t' == int) then Some(i, i') else helper_sum ((i,h)::t'') int
     |[]-> None)
   |[]-> None;;
 
-let another_one arr int =
-  if (two_sixm arr int != None) then
-    two_sixm arr int
- else another_one (drop_one arr) int;;
+let rec two_sum arr int =
+  if (helper_sum arr int != None) then
+    helper_sum arr int
+  else two_sum (drop_first arr) int;;
 
+let two_sum arr int = another_one (tuplefy arr) int;;
+
+two_sum [1;2;3] 5;;
+two_sum [2;7;11;15] 9;;
+two_sum [0;7;8;20] 27;;
+two_sum [1;2;3;4;5;6;7;8;9] 17;;
 
   
