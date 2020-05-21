77532eae12697ba07935720c1b5a5349e139050e
merrymeerkat <54228026+merrymeerkat@users.noreply.github.com>
2020-05-21 04:55:06 +0200

    Update container_with_most_water.ml

--- container_with_most_water.ml
+++ container_with_most_water.ml
@@ -13,11 +13,39 @@ let tuplefy l =
   |[] -> []
   in helper l 0;;
   
-  let max_int l =
-  ;
-  
-  let drop_max_int l =
-  ;
-  
-  let largest_container l =
-  ;
+let max_int l =
+  let rec helper l acc =
+    match l with
+    |h::t -> if h > acc then helper t h else helper t acc
+    |[] -> acc
+  in helper l 0;;
+
+(*let max_int l =
+  let rec helper l acc =
+    match l with
+    |(i,h) :: t -> if h > snd(acc) then helper t (i,h) else helper t acc
+    |[] -> acc
+  in helper l (0,0);;*)
+
+
+(*let rec drop int l =
+  match l with
+  |h::t -> if h == int then t else h :: drop int t
+  |[] -> [];;*)
+
+let rec drop int l =
+  match l with
+  |(i, h)::t -> if h == snd(int) then t else (h,i) :: drop int t
+  |[] -> [];;
+
+drop (1,2) [(0,1);(1,2);(2,3)];;
+
+let largest_container_helper l =
+  let distance = abs(fst(max_int l) - fst(max_int (drop (max_int l) l)))
+      in distance * snd(max_int (drop (max_int l) l));;
+
+let largest_container l = largest_container_helper (tuplefy l);;
+
+largest_container [1;8;6;2;5;4;8;3;7];;
+                                    
+  (* get the distance between the two max ints and multiply it by the height of the smallest max*)
