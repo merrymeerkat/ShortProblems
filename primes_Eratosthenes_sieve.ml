cf1196db041b3e41ecb714e12c6a49b37c09f475
merrymeerkat <54228026+merrymeerkat@users.noreply.github.com>
2020-05-25 19:22:56 -0300

    Delete primes_Eratosthenes_sieve.ml

--- primes_Eratosthenes_sieve.ml
+++
@@ -1,9 +0,0 @@
-let find_primes n =
-  let l = list_generator n in
- let rec sieve l' primes =
-    (match l' with
-      [] -> primes
-     |h::t ->
-       let rest = (filter (fun x -> x mod h > 0) t) in
-       sieve rest (primes @ [h]))
- in sieve l [];;
