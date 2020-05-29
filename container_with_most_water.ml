68313dd26dbbc201562c2ae0cb1cf5c9e13b1bcb
merrymeerkat <54228026+merrymeerkat@users.noreply.github.com>
2020-05-29 22:04:49 +0200

    Update container_with_most_water.ml

--- container_with_most_water.ml
+++ container_with_most_water.ml
@@ -12,47 +12,6 @@ let tuplefy l =
   |h::t -> (counter, h) :: helper t (counter + 1)
   |[] -> []
   in helper l 0;;
-  
-let max_int l =
-  let rec helper l acc =
-    match l with
-    |(i,h) :: t -> if h > snd(acc) then helper t (i,h) else helper t acc
-    |[] -> acc
-  in helper l (0,0);;
-
-let rec drop int l =
-  match l with
-  |(i, h)::t -> if h == snd(int) then t else (h,i) :: drop int t
-  |[] -> [];;
-
-drop (1,2) [(0,1);(1,2);(2,3)];;
-
-let largest_container_helper l =
-  let distance = abs(fst(max_int l) - fst(max_int (drop (max_int l) l)))
-      in distance * snd(max_int (drop (max_int l) l));;
-
-let largest_container l = largest_container_helper (tuplefy l);;
-
-largest_container [1;8;6;2;5;4;8;3;7];;
-
-
-(* ... *)
-
-let biggest_container_with_first l =
-  let rec helper l acc =
-    match l with
-    |(i, h) :: t ->
-      (match t with
-      |(i', h') :: t' -> (if ((i' - i) * (min h h')) > acc
-                        then (helper ((i, h) :: t') ((i' - i) * (min h h')))
-                           else helper ((i, h) :: t') acc)
-      |[] -> acc)
-    |[] -> acc
-  in helper (tuplefy l) 0;;
-
-
-biggest_container_with_first [1;2;2;3];;
-
 
 (*Helper function. Given an indexed list (e.g. [(0,3);(1,3);(2,7)], it returns the area of the largest possible container formed between the first tuple of the list and any other tuple. *)
 let largest_with_first l =
@@ -67,12 +26,10 @@ let largest_with_first l =
     |[] -> acc
   in helper l 0;;
 
-
 largest_with_first [(0,2);(1,2);(2,3)];;
 
 (* Our final function: given an int list, returns the area of the largest possible container *)
-(* This works by transforming the input list into a tuple list 
-(in order to keep track of the order of elements) and iterating through the tuple list with the largest_with_first function. This means that we will compute the area of the largest container formed with the first element of the list, then discard this element and do the same on and on, until we reach an empty list. An accumulator variable stores the highest area seen so far, and the accumulator is returned once the list has been searched through completely. *)
+(* This works by transforming the input list into a tuple list (in order to keep track of the order of elements) and iterating through the tuple list with the largest_with_first function. This means that we will compute the area of the largest container formed with the first element of the list, then discard this element and do the same for the new first element, and so on, until we reach an empty list. An accumulator variable stores the highest area seen so far, and the accumulator is returned once the list has been searched through completely. *)
 let largest l =
   let rec helper l acc =
     let first_container = (largest_with_first l) in
@@ -83,7 +40,11 @@ let largest l =
     |[] -> acc)   
   in helper (tuplefy l) 0;;
 
+(* tests *)
 largest [1;8;6;2;5;4;8;3;7];;
+largest [0;0];;
+largest [1;2;3];;
+largest [44;44];;
 
 
                                     
