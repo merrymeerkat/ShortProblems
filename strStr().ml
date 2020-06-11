0005c7d53b384a9443054a6f0793778293c9aeac
merrymeerkat <54228026+merrymeerkat@users.noreply.github.com>
2020-06-11 04:18:40 +0200

    Update strStr().ml

--- strStr().ml
+++ strStr().ml
@@ -4,13 +4,10 @@
 
 (* Return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack. *)
 
-
-(* given a haystack and a needle, checks whether the first element of the needle is present in the haystack. If so, returns the index of this element in the haystack. Else returns -1 *)
-let check_first hst ndl =
-   let rec helper hst ndl counter =         
-  match counter with
-  |(-1) -> ~-1
-  |_ -> if (String.get hst counter) == (String.get ndl 0)
-        then counter
-        else helper hst ndl (counter - 1)
-    in helper hst ndl (String.length hst - 1);;
+(*Converts string into char list (to make use of OCaml's list pattern matching) *)
+let string_to_list str =
+   let length = String.length str in
+      let rec helper str counter acc =
+         if counter < 0 then acc
+          else helper str (counter - 1) (str.[counter] :: acc)
+      in helper str (length - 1) [];;
