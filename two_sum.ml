61e8c04d40491190093fb663d012d8e5e8a3856e
merrymeerkat <54228026+merrymeerkat@users.noreply.github.com>
2020-07-12 17:22:38 +0200

    Update two_sum.ml

--- two_sum.ml
+++ two_sum.ml
@@ -1,8 +1,8 @@
-(*Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+(* Given an array of integers, return indices of the two numbers such that they add up to a specific target. *)
 
-You may assume that each input would have exactly one solution, and you may not use the same element twice.
+(* You may assume that each input would have exactly one solution, and you may not use the same element twice. *)
 
-Level: Easy*)
+(* Level: Easy *)
 
 (*drops first element of a tuple list*)
 let rec drop_first tuple_list =
